e96752341916a95373f64e6bc570491e4909aebc
Giliard <giliardanaildo@gmail.com>
2026-01-30 20:47:53 -0300

    Adicionado função que mostra filmes na tela

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputArea = document.getElementById('busca-filme');
 const btnBuscar = document.getElementById('botao-buscar');
 const areaFilmes = document.getElementById('movies');
+const textoBoasVindas = document.querySelector('.texto-boas-vindas');
 
 async function buscarFilmes(nome) {
     const baseUrl = 'https://www.omdbapi.com/?apikey=';
@@ -18,9 +19,26 @@ async function buscarFilmes(nome) {
     try {
         const resposta = await fetch(`${baseUrl}${apikey}&s=${nomeCode}`);
         const dados = await resposta.json();
+        if(dados.Response === 'False'){
+            alert('Nenhum filme encontrado com esse nome.');
+            return;
+        }
         
-        console.log(dados);
+        dados.Search.forEach(filme => {
 
+            const card = document.createElement('div');
+            const cardTitulo = document.createElement('h3');
+            const cardAno = document.createElement('p');
+            card.classList.add('filme-card');
+            cardTitulo.classList.add('filme-titulo');
+            cardAno.classList.add('filme-ano')
+            cardTitulo.textContent = filme.Title;
+            cardAno.textContent = filme.Year;
+            areaFilmes.appendChild(card);
+            card.appendChild(cardTitulo);
+            card.appendChild(cardAno);
+
+        });
     }
     catch (erro) {
         console.log('Erro na requisição: ' + erro.message);
@@ -29,6 +47,8 @@ async function buscarFilmes(nome) {
 
 btnBuscar.addEventListener('click' , (event) => {
     event.preventDefault();
+    textoBoasVindas.style.display = 'none';
+    areaFilmes.innerHTML = '';
     buscarFilmes(inputArea.value);
 })
 
